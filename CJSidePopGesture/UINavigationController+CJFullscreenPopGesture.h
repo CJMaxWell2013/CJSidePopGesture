87f06a7d9f29fc532e2ae4c47539e3d72ce83a72
J.Cheng <1875214064@qq.com>
2018-05-02 09:38:26 +0800

    初始化文件

---
+++ CJSidePopGesture/UINavigationController+CJFullscreenPopGesture.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+CJFullscreenPopGesture.h
+//  CJSidePopGestureDemo
+//
+//  Created by J.Cheng on 2018/5/2.
+//  Copyright © 2018年 北极星电力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (CJFullscreenPopGesture)
+
+@end
---
+++ CJSidePopGesture/UINavigationController+CJFullscreenPopGesture.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+CJFullscreenPopGesture.m
+//  CJSidePopGestureDemo
+//
+//  Created by J.Cheng on 2018/5/2.
+//  Copyright © 2018年 北极星电力. All rights reserved.
+//
+
+#import "UINavigationController+CJFullscreenPopGesture.h"
+
+@implementation UINavigationController (CJFullscreenPopGesture)
+
+@end
